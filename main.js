35d34da745f89eca6f6b9f88ecb8207261496d59
Tim Lee <TLee@Tims-MacBook-Air.local>
2016-10-05 17:05:27 -0700

    completed

---
+++ main.js
@@ -0,0 +1,136 @@
+// //1 Show me how to calculate the average price of all items.
+var price = 0;
+var avgPrice = 0;
+items.forEach(function(b) {
+	price += b.price;
+	avgPrice = price/items.length
+
+});
+var answer1 = "Average price is $" + avgPrice.toFixed(2)
+
+document.getElementById('answer1').innerHTML = answer1
+
+
+//2 Show me how to get an array of items that cost between $14.00 and $18.00 USD
+
+var filterItems = [];
+
+items.filter(function(c) {
+	if (c.price > 14 && c.price < 18) {
+		filterItems.push(c.title)
+	}
+})
+
+document.getElementById('answer2').innerHTML = filterItems
+
+
+//3 Which item has a "GBP" currency code? Display it's name and price.
+
+var gbpItems = items.filter(function(d) {
+	return d.currency_code === "GBP"
+})
+
+document.getElementById('answer3').innerHTML = gbpItems
+
+
+//4 Display a list of all items who are made of wood.
+
+var woodItems = items.filter(function(e) {
+	return e.materials.indexOf("wood") !== -1
+})
+
+var woodHtml = ""
+
+var woodItemNames = woodItems.forEach(function(m) {
+	woodHtml += "<ul>"
+	woodHtml += "<li>" + m.title + "</li>"
+	woodHtml += "</ul>"
+})
+
+document.getElementById("answer4").innerHTML = woodHtml
+
+//5 Which items are made of eight or more materials? Display the name, number of items and the items it is made of.
+
+var complexItems = items.filter(function(k) {
+	return k.materials.length > 8
+})
+
+complexHTML = "";
+
+complexItems.forEach(function(l) {
+	complexHTML += "<ul>"
+	complexHTML += "<li>" + l.title + "</li>"
+	l.materials.forEach(function(material){
+		complexHTML += "<li>" + material + "</li>"
+	})
+	complexHTML += "</ul>"
+})
+
+document.getElementById("answer5").innerHTML = complexHTML
+
+
+//6 How many items were made by their sellers?
+
+
+var madeBy = items.filter(function(g) {
+	return g.who_made === "i_did" 
+})
+
+document.getElementById("answer6").innerHTML = madeBy.length + " items were made by their sellers."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var longestName = arr.reduce(function(a,b)) {
+// 	if (a.longest == undefined) {
+// 		if (a.length > b.length) {
+// 			return {longestName:a}
+// 		}
+// 		else {
+// 			return {longestName:b}
+// 		}
+// 	}
+// 	else {
+// 		if (a.longestName.length > b.length) {
+// 			return a
+// 		}
+// 		else {
+// 			return {longestName:b}
+// 		}
+// 	}
+// }
+
+
+
+// var longestName = arr.reduce(function(a,b) {}, {longestName:''})
+
+// document.geteleemntbyid.innerhtml = variable answer
+
+
